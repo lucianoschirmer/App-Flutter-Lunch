ffa90121a4ed080547ec4aa7b20ba4ecab3aa3cf
lucianoschirmer <lucianoschirmerbrasil@gmail.com>
2019-11-26 09:46:46 -0300

    first commit

---
+++ lib/fitness_app/models/meals_list_data.dart
@@ -0,0 +1,52 @@
+class MealsListData {
+  MealsListData({
+    this.imagePath = '',
+    this.titleTxt = '',
+    this.startColor = '',
+    this.endColor = '',
+    this.meals,
+    this.kacl = 0,
+  });
+
+  String imagePath;
+  String titleTxt;
+  String startColor;
+  String endColor;
+  List<String> meals;
+  int kacl;
+
+  static List<MealsListData> tabIconsList = <MealsListData>[
+    MealsListData(
+      imagePath: 'assets/fitness_app/breakfast.png',
+      titleTxt: 'Pão com Ovo',
+      kacl: 525,
+      meals: <String>['Pão,', 'Banha,', 'Ovo'],
+      startColor: '#FA7D82',
+      endColor: '#FFB295',
+    ),
+    MealsListData(
+      imagePath: 'assets/fitness_app/lunch.png',
+      titleTxt: 'Lasanha',
+      kacl: 602,
+      meals: <String>['Camarão,', '4 Queijos,', 'Carne'],
+      startColor: '#738AE6',
+      endColor: '#5C5EDD',
+    ),
+    MealsListData(
+      imagePath: 'assets/fitness_app/snack.png',
+      titleTxt: 'Vegan',
+      kacl: 0,
+      meals: <String>['Grama:', 'Tomate'],
+      startColor: '#FE95B6',
+      endColor: '#FF5287',
+    ),
+    MealsListData(
+      imagePath: 'assets/fitness_app/dinner.png',
+      titleTxt: 'Costela',
+      kacl: 0,
+      meals: <String>['Recommend:', '703 kcal'],
+      startColor: '#6F72CA',
+      endColor: '#1E1466',
+    ),
+  ];
+}
